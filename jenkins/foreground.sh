e360d95e9a7fa3727dcb3a0dda627a7903e9eb3f
Anil Kumle <anil.kumle@sakhaglobal.com>
2022-08-24 14:55:42 +0530

    added

--- jenkins/foreground.sh
+++ jenkins/foreground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://anilkumle1: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@github.com/anilkumle1/java.git
+git clone https://github.com/anilkumle1/java.git
 
 cp -r java/jenkins_home /var/lib/docker/volumes
 sleep 2
@@ -8,10 +8,17 @@ chown -R ubuntu:ubuntu /var/lib/docker/volumes/jenkins_home
 
 docker login -u anilkumle -p Athrz_lgn@123
 
-cd /var/lib/docker/volumes && chown -R ubuntu:ubuntu jenkins_home
+# cd /var/lib/docker/volumes && chown -R ubuntu:ubuntu jenkins_home
 
-docker run -itd -p 8080:8080 -v jenkins_home:/var/jenkins_home \
+docker run -itd --name jenkins_server -p 8080:8080 -v jenkins_home:/var/jenkins_home \
      -v /var/run/docker.sock:/var/run/docker.sock \
        -v $(which docker):/usr/bin/docker anilkumle/jenkins:aug23
+
+docker stop jenkins_server
+
+cd /var/lib/docker/volumes && chown -R ubuntu:ubuntu jenkins_home
+
+docker restart jenkins_server
+
 
 echo "jenkins is up and running"
